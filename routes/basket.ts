4d1e0b3e4b6b4f923301dd8c0902e9af0a5b693e
Oshadi123342 <adithyaoshadi46@gmail.com>
2026-09-27 00:42:44 +0530

    Fix basket IDOR with ownership validation

--- routes/basket.ts
+++ routes/basket.ts
@@ -6,22 +6,55 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { ProductModel } from '../models/product'
 import { BasketModel } from '../models/basket'
-import * as challengeUtils from '../lib/challengeUtils'
 
 import * as utils from '../lib/utils'
 import * as security from '../lib/insecurity'
-import { challenges } from '../data/datacache'
 
 export function retrieveBasket () {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const id = req.params.id
-      const basket = await BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
-      /* jshint eqeqeq:false */
-      challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
-        const user = security.authenticatedUsers.from(req)
-        return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user?.bid != parseInt(id, 10) // eslint-disable-line eqeqeq
+      // Get the currently authenticated user
+      const user = security.authenticatedUsers.from(req)
+
+      // Reject request if no authenticated user or basket exists
+      if (!user?.data?.id || !user.bid) {
+        res.status(401).json({
+          error: 'Authentication required'
+        })
+        return
+      }
+
+      // Get requested basket ID
+      const id = Number(req.params.id)
+
+      // Validate basket ID and check basket ownership
+      if (
+        !Number.isSafeInteger(id) ||
+        id < 1 ||
+        Number(user.bid) !== id
+      ) {
+        res.status(403).json({
+          error: 'Forbidden'
+        })
+        return
+      }
+
+      // Retrieve basket only if it belongs to the authenticated user
+      const basket = await BasketModel.findOne({
+        where: {
+          id,
+          UserId: user.data.id
+        },
+        include: [
+          {
+            model: ProductModel,
+            paranoid: false,
+            as: 'Products'
+          }
+        ]
       })
+
+      // Translate product names
       if (((basket?.Products) != null) && basket.Products.length > 0) {
         for (let i = 0; i < basket.Products.length; i++) {
           basket.Products[i].name = req.__(basket.Products[i].name)
@@ -34,3 +67,4 @@ export function retrieveBasket () {
     }
   }
 }
+
